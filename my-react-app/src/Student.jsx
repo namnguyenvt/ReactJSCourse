fd01e5ff690b0b81b84f30306f93f6b00079dbc4
Nathan NGUYEN <22069149@student.westernsydney.edu.au>
2024-11-28 13:56:33 +1100

    props part 2

--- my-react-app/src/Student.jsx
+++ my-react-app/src/Student.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 
 function Student(props) {
 
@@ -9,5 +10,15 @@ function Student(props) {
         </div>
     );
 }
+Student.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool,
+}
 
+Student.defaultProps = {
+    name: "Guest",
+    age: 0,
+    isStudent: false,
+}
 export default Student
